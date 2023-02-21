94328da2aecc7e10eaed8d862179143ef615722f
SteelRay013 <aneesh.anil013@gmail.com>
2023-02-21 16:11:15 -0500

    js search

--- Geturcar.js
+++ Geturcar.js
@@ -1,4 +1,7 @@
 function Search() {//Search
+const sedan = document.getElementById("Sedan"),
+suv = document.getElementById("SUV"),
+showall= document.getElementById ("Show_all");
 let input = document.getElementById('myInput').value
 input=input.toLowerCase();
 let x = document.getElementsByTagName('article');
@@ -6,46 +9,67 @@ for (i = 0; i < x.length; i++) {
 if(!x[i].innerHTML.toLowerCase().includes(input)){
         x[i].style.display="none";}           
     }
+    if (sedan.classList.contains('active2')){
+      Sedan();
+    }
+    if (suv.classList.contains('active2')){
+      SUV();
+    }
+    if (showall.classList.contains('active2')){
+      Showall();
+    }
+}
+function Search2() {//Search
+
+  let input = document.getElementById('myInput').value
+  input=input.toLowerCase();
+  let x = document.getElementsByTagName('article');
+  for (i = 0; i < x.length; i++) {
+  if(!x[i].innerHTML.toLowerCase().includes(input)){
+          x[i].style.display="none";}           
+      }
 }
 
 
 function Sedan() {//Show Sedan
-  var element = document.getElementById("Sedan");
-  element.classList.add("active2");
-  var element = document.getElementById("SUV");
-  element.classList.remove("active2");
-  var element= document.getElementById ("Show_all");
-  element.classList.remove('active2')
-  var element=document.getElementsByClassName("ShowSUV")
+  const sedan = document.getElementById("Sedan"),
+  suv = document.getElementById("SUV"),
+  showall= document.getElementById ("Show_all");
+  sedan.classList.add("active2");
+  suv.classList.remove("active2");
+  showall.classList.remove('active2');
+  var element=document.getElementsByClassName("ShowSUV");
   for (let elements of element) { elements.style.display = "none"; }
   var element=document.getElementsByClassName("ShowSedan");
   for (let elements of element) { elements.style.display = ""; }
-  Search();
+  Search2();
+  return
 }
 function SUV() {//Show SUV
-
-  var element = document.getElementById("Sedan");
-  element.classList.remove("active2");
-  var element = document.getElementById("SUV");
-  element.classList.add("active2");
-  var element= document.getElementById ("Show_all");
-  element.classList.remove('active2')
+  const sedan = document.getElementById("Sedan"),
+  suv = document.getElementById("SUV"),
+  showall= document.getElementById ("Show_all");
+  sedan.classList.remove("active2");
+  suv.classList.add("active2");
+  showall.classList.remove('active2')
   var element=document.getElementsByClassName("ShowSedan")
   for (let elements of element) { elements.style.display = "none"; }
   var element=document.getElementsByClassName("ShowSUV");
   for (let elements of element) { elements.style.display = ""; }
-  Search();
+  Search2();
+  return
 }
 function Showall(){//Show all
-  var element = document.getElementById("Sedan");
-  element.classList.remove("active2");
-  var element = document.getElementById("SUV");
-  element.classList.remove("active2");  
-  var element= document.getElementById ("Show_all");
-  element.classList.add('active2')
+  const sedan = document.getElementById("Sedan"),
+  suv = document.getElementById("SUV"),
+  showall= document.getElementById ("Show_all");
+  sedan.classList.remove("active2");
+  suv.classList.remove("active2");  
+  showall.classList.add('active2')
   var element=document.getElementsByClassName("ShowSedan")
   for (let elements of element) { elements.style.display = ""; }
   var element=document.getElementsByClassName("ShowSUV");
   for (let elements of element) { elements.style.display = ""; }
-  Search();
+  Search2();
+  return
 }
